5acfe4e71fdb6112f8a1b74374e74a81e13393e7
Martin Brandl <mbr@whiteduck.local>
2017-05-16 13:07:41 +0200

    updated to .net standard

---
+++ Swagger.AADExtension.Core/SwaggerUIOptionsAADExtension.cs
@@ -0,0 +1,25 @@
+﻿using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Collections.Generic;
+
+namespace Whiteduck.Swagger.AADExtension
+{
+    public static class SwaggerUIOptionsAADExtension
+    {
+        /// <summary>
+        /// Sets specific SwaggerUIOptions for authenticating against Azure Active Directory.
+        /// </summary>
+        /// <param name="option"></param>
+        /// <param name="swaggerClientId">The client id of your swagger client.</param>
+        /// <param name="apiClientId">The client id of your API.</param>
+        public static void ConfigureAAD(this SwaggerUIOptions option, string swaggerClientId, string apiClientId)
+        {
+            var resourceDictionary = new Dictionary<string, string>
+            {
+                { "resource", apiClientId }
+            };
+
+            option.ConfigureOAuth2(swaggerClientId, null, null, swaggerClientId, string.Empty, resourceDictionary);
+        }
+        
+    }
+}
