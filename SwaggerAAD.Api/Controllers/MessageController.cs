1af41b2d771ef9fc6a87288bd29781573a0bc2eb
Martin Brandl <mbr@whiteduck.local>
2017-05-17 09:18:37 +0200

    added example project

---
+++ SwaggerAAD.Api/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace SwaggerAAD.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class MessageController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello world.";
+        }
+    }
+}
